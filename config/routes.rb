2f6fd4e2881dcc43c735546960c25d43a80f32a4
Anurag Pandey <lapmid258@gmail.com>
2018-09-06 23:35:09 +0530

    remove registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   resources :doubts
-  devise_for :users
+  devise_for :users, skip: [:registrations]
+  as :user do
+    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
   
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'doubts#new'
+
+    
 end
